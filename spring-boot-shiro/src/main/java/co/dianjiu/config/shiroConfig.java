fa9128477cd62730e299b06874d9336b33168685
dianjiu <mw961124>
2020-09-07 18:17:51 +0800

    优化注解说明

--- spring-boot-shiro/src/main/java/co/dianjiu/config/shiroConfig.java
+++ spring-boot-shiro/src/main/java/co/dianjiu/config/shiroConfig.java
@@ -16,6 +16,10 @@ import java.util.Map;
 @Configuration
 public class shiroConfig {
 
+    /**
+     * 使用事务拦截器方式创建代理配置
+     * @return
+     */
     @Bean
     @ConditionalOnMissingBean
     public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
@@ -24,14 +28,20 @@ public class shiroConfig {
         return defaultAAP;
     }
 
-    //将自己的验证方式加入容器
+    /**
+     * 将自定义的验证方式加入容器
+     * @return
+     */
     @Bean
     public CustomRealm myShiroRealm() {
         CustomRealm customRealm = new CustomRealm();
         return customRealm;
     }
 
-    //权限管理，配置主要是Realm的管理认证
+    /**
+     * 权限管理，配置主要是Realm的管理认证
+     * @return
+     */
     @Bean
     public DefaultWebSecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
@@ -39,7 +49,11 @@ public class shiroConfig {
         return securityManager;
     }
 
-    //Filter工厂，设置对应的过滤条件和跳转条件
+    /**
+     * Filter工厂，设置对应的过滤条件和跳转条件
+     * @param securityManager
+     * @return
+     */
     @Bean
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
@@ -64,7 +78,13 @@ public class shiroConfig {
         return shiroFilterFactoryBean;
     }
 
-    // 加入注解的使用，不加入这个注解不生效
+    /**
+     * 加入注解的使用，不加入这个注解不生效
+     * 例如： 在Controller中使用如下注解 接口需要新增权限
+     * @RequiresPermissions("add")
+     * @param securityManager
+     * @return
+     */
     @Bean
     public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
