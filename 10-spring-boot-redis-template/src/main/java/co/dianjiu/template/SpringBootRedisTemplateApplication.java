14d001fcecb6eed01ed185b7d165a03ffad9673a
dianjiu <mail@dianjiu.cc>
2020-10-19 17:31:29 +0800

    更新目录结构

---
+++ 10-spring-boot-redis-template/src/main/java/co/dianjiu/template/SpringBootRedisTemplateApplication.java
@@ -0,0 +1,14 @@
+package co.dianjiu.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisTemplateApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("cfg.env", "local");
+        SpringApplication.run(SpringBootRedisTemplateApplication.class, args);
+    }
+
+}
