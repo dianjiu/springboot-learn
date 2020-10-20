e9fdaa8e0b0d956ab9fdb381a5c7c44be0e8d592
dianjiu <mail@dianjiu.cc>
2020-10-20 09:30:05 +0800

    测试热部署

--- 02-spring-boot-hello/src/main/java/co/dianjiu/hello/controller/HelloController.java
+++ 02-spring-boot-hello/src/main/java/co/dianjiu/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package co.dianjiu.hello.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ public class HelloController {
     }
 
 
-    @GetMapping("/sourceC/{source_name}")
+    @GetMapping("/welcome/{source_name}")
     public String sourceC(@PathVariable("source_name") String sourceName){
-        return "你正在访问sourceC资源";
+        return "你正在访问sourceC资源。。。";
     }
 }
